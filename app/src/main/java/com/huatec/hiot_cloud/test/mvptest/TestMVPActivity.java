5628654c2dbbf6c4300d17ef252a18a0241bd68a
何坚栋 <1263067775@qq.com>
2021-04-07 02:05:21 +0800

    初步登录界面

--- app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestMVPActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/test/mvptest/TestMVPActivity.java
@@ -3,12 +3,43 @@ package com.huatec.hiot_cloud.test.mvptest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.huatec.hiot_cloud.R;
+import com.huatec.hiot_cloud.test.mvptest.module.User;
 
 public class TestMVPActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_mvp);
+        Log.d("tag","test");
+        final EditText etUerName = findViewById(R.id.et_user_name);
+        final EditText etPassword = findViewById(R.id.et_password);
+        Button btnLogin = findViewById(R.id.btn_login);
+        final User user = new User();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            //登录
+            @Override
+            public void onClick(View v) {
+                //mvc 做法，在这做身份校验
+                user.setUserName(etUerName.getText().toString());
+                user.setPassword(etPassword.getText().toString());
+                login(user);
+            }
+        });
+    }
+
+    private void login(User user) {
+        if("hjd".equals(user.getUserName()) && "29".equals(user.getPassword())){
+            Toast.makeText(this, "登录成功",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "登录失败",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/huatec/hiot_cloud/test/mvptest/module/User.java
+++ app/src/main/java/com/huatec/hiot_cloud/test/mvptest/module/User.java
@@ -1,4 +1,27 @@
 package com.huatec.hiot_cloud.test.mvptest.module;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+
 }
